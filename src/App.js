c2be12993d4ffbae7e36daf82bc34994e2988d60
Anoplay <agungprasetyo1111@gmail.com>
2018-12-04 09:58:54 +0700

    yada vs sheinlong

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Anoplay tea
+            yada93 vs sheinlong77
           </a>
         </header>
       </div>
