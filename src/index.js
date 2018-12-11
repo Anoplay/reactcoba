267dbbe85deb31cb84c63ec3eb82bc05aed04cbe
Anoplay <agungprasetyo1111@gmail.com>
2018-12-11 09:15:14 +0700

    menambahkan ckeditor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// module.exports = require('./ckeditor');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
